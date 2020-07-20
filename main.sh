f3202282ff7e102cd8b4e7a9a78912deabf10b2b
Risingfeanyx <35235934+Risingfeanyx@users.noreply.github.com>
2020-07-20 08:38:00 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,19 +1,23 @@
 	#/bin/bash
-	
-	#Wordpress
-	if test -f "wp-config.php"; then echo "This is Wordpress $(wp core version)" ;fi 
+
+	Wordpress()
+	{
+	clear
+	echo "This is Wordpress $(wp core version)"
 	echo "Password for $(awk -F"'" '/DB_NAME/{print $4}' wp-config.php) is $(awk -F"'" '/DB_PASSWORD/{print $4}' wp-config.php)"
 	echo "mysqldump -p -u $(awk -F"'" '/DB_USER/{print $4}' wp-config.php) $(awk -F"'" '/DB_NAME/{print $4}' wp-config.php) > $(awk -F"'" '/DB_NAME/{print $4}' wp-config.php).$(whoami).$(date +%F).sql"
 	ls -1t | head -1 | grep *.sql
-	
+	}
 
-
-	#Prestashop
-	
+	Prestashop()
+	{
+	clear
 	if test -f "config/settings.inc.php"; then echo "This is Prestashop $(awk -F"'" '/PS_VERSION/{print $4}' config/settings.inc.php) installed on $(awk -F"'" '/PS_CREATION_DATE/{print $4}' config/settings.inc.php)" ;fi 
 	echo "Password for $(awk -F"'" '/DB_NAME/{print $4}' config/settings.inc.php) is $(awk -F"'" '/DB_PASSWD_/{print $4}' config/settings.inc.php)"
 	echo "mysqldump -p -u $(awk -F"'" '/DB_USER/{print $4}' config/settings.inc.php) $(awk -F"'" '/DB_NAME/{print $4}' config/settings.inc.php) > $(awk -F"'" '/DB_NAME/{print $4}' config/settings.inc.php).$(whoami).$(date +%F).sql"
 	ls -1t | head -1 | grep *.sql
+	}
+
 
 	#Joomla
 	
