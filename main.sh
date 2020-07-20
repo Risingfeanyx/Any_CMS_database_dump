6ec8dd2a061dcab9c1827cf8b01f35ac31b6c8b7
Risingfeanyx <35235934+Risingfeanyx@users.noreply.github.com>
2020-07-20 08:25:32 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 	#/bin/bash
-	clear
+	
 	#Wordpress
 	if test -f "wp-config.php"; then echo "This is Wordpress $(wp core version)" ;fi 
 	echo "Password for $(awk -F"'" '/DB_NAME/{print $4}' wp-config.php) is $(awk -F"'" '/DB_PASSWORD/{print $4}' wp-config.php)"
@@ -9,7 +9,7 @@
 
 
 	#Prestashop
-	clear
+	
 	if test -f "config/settings.inc.php"; then echo "This is Prestashop $(awk -F"'" '/PS_VERSION/{print $4}' config/settings.inc.php) ;fi
 	installed on $(awk -F"'" '/PS_CREATION_DATE/{print $4}' config/settings.inc.php)" ;fi 
 	echo "Password for $(awk -F"'" '/DB_NAME/{print $4}' config/settings.inc.php) is $(awk -F"'" '/DB_PASSWD_/{print $4}' config/settings.inc.php)"
