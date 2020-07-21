cd358da80d595897787e8978da8842e1d2e9e0f8
Risingfeanyx <35235934+Risingfeanyx@users.noreply.github.com>
2020-07-21 07:32:17 -0400

    Create Wordpress.sh

---
+++ Wordpress.sh
@@ -0,0 +1,5 @@
+	#!/bin/bash
+	echo "This is Wordpress $(wp core version)"
+	echo "Password for $(awk -F"'" '/DB_NAME/{print $4}' wp-config.php) is $(awk -F"'" '/DB_PASSWORD/{print $4}' wp-config.php)"
+	echo "mysqldump -p -u $(awk -F"'" '/DB_USER/{print $4}' wp-config.php) $(awk -F"'" '/DB_NAME/{print $4}' wp-config.php) > $(awk -F"'" '/DB_NAME/{print $4}' wp-config.php).$(whoami).$(date +%F).sql"
+	ls -1t | head -1 | grep *.sql
